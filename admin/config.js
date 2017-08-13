ea91afcc7937baf317ce4546c0414a0d9e4b01c9
Utsob Roy <uroybd@gmail.com>
2017-08-13 15:39:48 +0600

    from ashes I rise [HEAD]

---
+++ admin/config.js
@@ -0,0 +1,5 @@
+//Place here your personal Access Token, you can go to our Readme if you need help
+	var AccessToken = 'c69f6b2e866726d50817007a75fe32dc454fd1d3';
+
+//Html tab size
+	var htmlEditorTabSize = '2';
